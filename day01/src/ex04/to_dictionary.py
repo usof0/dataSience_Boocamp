5b88b41e7fdcfd1629918b067a70156faf06f6c4
Renly Katharina <renlykat@student.21-school.ru>
2025-02-04 07:33:24 +0300

    ex04 optimized

--- day01/src/ex04/to_dictionary.py
+++ day01/src/ex04/to_dictionary.py
@@ -1,4 +1,19 @@
-def to_dict():
+def to_dict(list_of_tuples):
+    dictionary = {}
+    for country, number in list_of_tuples:
+        if number in dictionary:
+            dictionary[number].append(country)
+        else:
+            dictionary[number] = [country]
+        
+    return dictionary
+
+def output(dictionary):
+    for number, countries in dictionary.items():
+        for country in countries:
+            print(f"'{number}' : '{country}'")
+
+def main():
     list_of_tuples = [
         ('Russia', '25'),
         ('France', '132'),
@@ -21,23 +36,8 @@ def to_dict():
         ('Austria', '14'),
         ('Israel', '12')
     ]
-
-    dictionary = {}
-    for country, number in list_of_tuples:
-        if number in dictionary:
-            dictionary[number].append(country)
-        else:
-            dictionary[number] = [country]
-        
-    return dictionary
-
-def output(dictionary):
-    for number, countries in dictionary.items():
-        for country in countries:
-            print(f"'{number}' : '{country}'")
-
-def main():
-    dictionary = to_dict()
+    
+    dictionary = to_dict(list_of_tuples)
     output(dictionary)
 
 if __name__ == "__main__":
