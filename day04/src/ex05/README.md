2634155abcac2c2a9a292c04a26600b00b28ab2a
Renly Katharina <renlykat@student.21-school.ru>
2025-02-24 19:37:36 +0300

    ex05

---
+++ day04/src/ex05/README.md
@@ -0,0 +1,2 @@
+you can download the dataset 'ratings.csv' from the link:
+https://mega.nz/file/lRwXXB7Z#BpZx0B7glJyx1sA83WpEAwODavqWgGfEyZ2mQQhTbS4
---
+++ day04/src/ex05/generator.py
@@ -0,0 +1,45 @@
+# python3 generator.py ratings.csv
+import os
+import sys
+import resource
+
+# we can use psutil module instead of resource 'it works on windows too'
+# import psutil
+# time = time.process_time()
+# mem = process.memory_info().rss
+
+def file_reader_with_generator(input_file):
+    with open(input_file, 'r') as file:
+        for line in file:
+            yield line
+
+def main():
+    try:
+        if len(sys.argv) != 2:
+            raise ValueError("Usage: python3 generator.py <input_file>")
+        
+        file_path = sys.argv[1]
+
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f"File {file_path} does not exist")
+        
+        start_time = resource.getrusage(resource.RUSAGE_SELF).ru_utime + resource.getrusage(resource.RUSAGE_SELF).ru_stime
+
+        lines = file_reader_with_generator(file_path)
+        for line in lines:
+            pass
+
+        end_time = resource.getrusage(resource.RUSAGE_SELF).ru_utime + resource.getrusage(resource.RUSAGE_SELF).ru_stime
+        end_mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+        peak_mem_usage = end_mem / (1024 * 1024)
+        elapsed_time = end_time - start_time
+
+        print(f"Peak Memory Usage = {peak_mem_usage:.3f} GB")
+        print(f"User Mode Time + System Moode Time = {elapsed_time:.3f}s")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
+    
